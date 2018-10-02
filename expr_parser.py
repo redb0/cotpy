018a9ef1383bf95d63b29ad450f9055b20339080
Vladimir <voronov.volodya2013@yandex.ru>
2018-10-02 11:58:54 +0700

    Переработан парсер имен

--- expr_parser.py
+++ expr_parser.py
@@ -1,16 +1,19 @@
 from typing import Tuple, Union
 
+
+# TODO: пока только односимвольные имена
 default_params = {
-    "var_obj": "x",
-    "var_control": "u",
-    "var_coefficient": "a",
-    "var_time": "t",
-    "var_error": "e",  # ошибка
-    "var_unknown_impact": "h",  # неизвестное воздействие
+    'var_obj': 'x',
+    'var_control': 'u',
+    'var_coefficient': 'a',
+    'var_time': 't',
+    'var_error': 'e',  # ошибка
+    'var_unknown_impact': 'h',  # неизвестное воздействие
 
     # ??????
-    "model": "y",  # модель
-    "delimiter": "_"
+    'model': 'y',  # модель
+    'delimiter': '_',
+    'default_idx': 0
 }
 
 OPERATORS = ('+', '-', '*', '/', '**')
@@ -21,27 +24,49 @@ OPERATORS = ('+', '-', '*', '/', '**')
 def parse_expr(expr: str, params=default_params):
     expr = expr.replace(' ', '')
     i = 0
+    # FIXME: last_idx
+    last_idx = 0
+    res = ''
     while i < len(expr):
         s = expr[i]
-        if s == params["var_obj"]:
+        name = ''
+        if s == params['var_obj']:
+            name, idx, tao, i = parse_var(expr, i, last_idx, params=default_params)
+            print('name =', name)
+            print('idx =', idx)
+            print('tao =', tao)
+            print('i =', i)
+        elif s == params['var_control']:
+            name, idx, tao, i = parse_var(expr, i, last_idx, params=default_params)
+            print('name =', name)
+            print('idx =', idx)
+            print('tao =', tao)
+            print('i =', i)
+        elif s == params['var_coefficient']:
+            name, idx, _, i = parse_var(expr, i, last_idx, params=default_params)
+            print('name =', name)
+            print('idx =', idx)
+            print('i =', i)
+        elif s == params['var_error']:
             pass
-        elif s == params["var_control"]:
+        elif s == params['var_unknown_impact']:
             pass
-        elif s == params["var_coefficient"]:
-            pass
-        elif s == params["var_error"]:
-            pass
-        elif s == params["var_unknown_impact"]:
-            pass
+        else:
+            res += s
+            i += 1
+        # if name:
+        res += name
+
+    return res
 
 
-def parse_var(expr: str, i: int, last_idx: int, delimiter: str='_',
+def parse_var(expr: str, i: int, last_idx: int,
               params=default_params) -> Union[Exception, Tuple[str, int, int, int]]:
     s = expr[i]
     idx = -1
     i += 1
     while i < len(expr) and expr[i] != ')':
-        if expr[i] == delimiter:
+        if expr[i] == params['delimiter']:
             i += 1
         elif expr[i].isdigit():
             idx, i = int_parse(expr, i)
@@ -49,7 +74,7 @@ def parse_var(expr: str, i: int, last_idx: int, delimiter: str='_',
             if s == params['var_coefficient']:
                 if idx == -1:
                     idx = last_idx + 1
-                name = s + delimiter + str(idx)
+                name = s + str(idx)  # params['delimiter']
                 return name, idx, 0, i
             elif s in (params['var_control'], params['var_obj']):
                 message = 'Некорректное указание временной задержки. Ожидается: "(". Текущий символ: ' + expr[i] + '.'
@@ -59,26 +84,29 @@ def parse_var(expr: str, i: int, last_idx: int, delimiter: str='_',
             if s in (params['var_control'], params['var_obj']):
                 tao, i = parse_lag(expr, i, params['var_time'])
                 if idx != -1:
-                    name = s + delimiter + str(idx)
+                    name = s + str(idx) + '_' + str(idx)
                 else:
-                    name = s + delimiter + str(tao)
+                    name = s + str(params['default_idx']) + '_' + str(tao)
                 return name, idx, tao, i
             elif s == params['var_coefficient']:
                 message = 'Указание временной задержки у коэффициентов не поддерживаеся.'
                 raise ValueError(message)
+        else:
+            message = get_error_message(reality=expr[i], position=i)
+            raise ValueError(message)
 
     if i >= len(expr):
         if s == params['var_coefficient']:
             if idx == -1:
                 idx = last_idx + 1
-            name = s + delimiter + str(idx)
+            name = s + str(idx)  # params['delimiter']
             return name, idx, 0, i
         elif s in (params['var_control'], params['var_obj']):
             message = 'Ожидается временная задержка'
             raise ValueError(message)
 
-    if expr[i] == ')':
-        pass
+    # if expr[i] == ')':
+    #     print('тут закрывающая скобка')
 
 
 def parse_lag(expr: str, i: int, var_time: str) -> Union[Exception, Tuple[int, int]]:
@@ -89,25 +117,25 @@ def parse_lag(expr: str, i: int, var_time: str) -> Union[Exception, Tuple[int, i
             if expr[i] == var_time:
                 i += 1
             else:
-                message = 'Ожидается "' + var_time + '". Текущий символ "' + expr[i] + '"'
+                message = get_error_message(expect=var_time, reality=expr[i], position=i)
                 raise ValueError(message)
         elif i - start == 1:
             if expr[i] == '-':
                 i += 1
             else:
-                message = 'Ожидается "' + var_time + '". Текущий символ "' + expr[i] + '"'
+                message = get_error_message(expect='"-"', reality=expr[i], position=i)
                 raise ValueError(message)
         elif i - start == 2:
             if expr[i].isdigit():
                 tao, i = int_parse(expr, i)
                 break
             else:
-                message = 'Ожидается величина задержки. Текущий символ "' + expr[i] + '"'
+                message = get_error_message(expect='величина задержки', reality=expr[i], position=i)
                 raise ValueError(message)
 
     if expr[i] == ')':
         return tao, i + 1
-    message = 'Ожидается ")". Текущий символ "' + expr[i] + '"'
+    message = get_error_message(expect='")"', reality=expr[i], position=i)
     raise ValueError(message)
 
 
@@ -119,45 +147,18 @@ def int_parse(expr: str, i: int) -> Tuple[int, int]:
     return int(numb), i
 
 
+def get_error_message(**kwargs) -> str:
+    if 'expect' in kwargs:
+        message = 'Ожидается ' + kwargs['expect']
+    else:
+        message = 'Неожиданный символ'
+    message += '. Текущий символ "' + kwargs['reality'] + '", позиция: ' + str(kwargs['position']) + '.'
+    return message
+
+
 def main():
-    s = 't-1)'
-    s1 = 't-25)'
-
-    s2 = 't-36.0)'
-    s3 = '-t1)'
-    s4 = 't-a1)'
-    s5 = 't-2s)'
-    s6 = '7t-)'
-
-    # tao, i = parse_lag(s6, 0, 't')
-    # print(tao, i)
-
-    v = 'x(t-1)'  # +
-    v1 = 'u(t-1)'  # +
-    v2 = 'x_0(t-1)'  # +
-    v3 = 'u_0(t-1)'  # +
-    v4 = 'a'  # +
-    v5 = 'a1'  # +
-    v6 = 'a_2'  # +
-
-    v7 = 'a(t-1)'  # +
-    v8 = 'a(t'  # +
-
-    v9 = 'x'  # +
-    v10 = 'u'  # +
-    v11 = 'x_0'  # +
-    v12 = 'u_0'  # +
-
-    v13 = 'x2(t-1)'  # +
-    v14 = 'u3(t-1)'  # +
-    v15 = 'x1(t-5)'  # +
-    v16 = 'u2(t-3)'  # +
-
-    # v9 = '3a'
-
-    name, idx, tao, i = parse_var(v4, 0, 0)
-    print(name, idx, tao, i)
-    # print(v[i])
+    r = parse_expr('a0*)x(t-1)+a_1*u(t-1)+a_2*(x(t-2)+u(t-2))')
+    print(r)
 
 if __name__ == '__main__':
     main()
