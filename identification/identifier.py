9efe37bcb6ec81f185bb0a1d0284638f644550e1
Vladimir <30475117+redb0@users.noreply.github.com>
2018-10-23 20:05:46 +0700

    небольшие изменения

--- identification/identifier.py
+++ identification/identifier.py
@@ -2,7 +2,7 @@ import support
 
 
 class Identifier:
-    def __init__(self, model, sigma=0):
+    def __init__(self, model, sigma=0.) -> None:
         self._model = model
 
         self._memory_len = 0
@@ -10,28 +10,18 @@ class Identifier:
 
     def min_numb_measurements(self):
         len_a = len(self._model.coefficients)
-        nm_u = [(i[0].name.split('_')[0], len_a + i[-1].tao) for i in self._model.inputs]
-        nm_x = [(i[0].name.split('_')[0], len_a + i[-1].tao) for i in self._model.outputs]
+        nm_u = [(i.group_name, len_a + i.max_tao) for i in self._model.inputs]
+        nm_x = [(i.group_name, len_a + i.max_tao) for i in self._model.outputs]
         return ('a', len_a), nm_x, nm_u
 
-    def init_zeros(self, n0=0):
+    def init_zeros(self, n0: int=0) -> None:
         len_a = len(self._model.coefficients)
         len_u = len(self._model.inputs)
         len_x = len(self._model.outputs)
         n0 = n0 if n0 >= len_a else len_a
-        # if not n0:
-        #     self._memory_len = len_a
-        # elif n0 and n0 >= len_a:
-        #     self._memory_len = n0
-        # else:
-        #     raise ValueError('Минимальное количество значений коэффициентов должно быть >= ' + str(len_a))
-        # a = [[0. for _ in range(self._memory_len)] for _ in range(len_a)]
-        # u = [[0. for _ in range(self._memory_len + self._model.inputs[i][-1].tao)] for i in range(len_u)]
-        # x = [[0. for _ in range(self._memory_len + self._model.outputs[i][-1].tao)] for i in range(len_x)]
-        # self.initialization(a, x, u, n0=self._memory_len)
         a = [[0. for _ in range(n0)] for _ in range(len_a)]
-        u = [[0. for _ in range(n0 + self._model.inputs[i][-1].tao)] for i in range(len_u)]
-        x = [[0. for _ in range(n0 + self._model.outputs[i][-1].tao)] for i in range(len_x)]
+        u = [[0. for _ in range(n0 + self._model.inputs[i].max_tao)] for i in range(len_u)]
+        x = [[0. for _ in range(n0 + self._model.outputs[i].max_tao)] for i in range(len_x)]
         self.initialization(a, x, u)
 
     def initialization(self, a, x, u) -> None:  # TODO: добавить типы
@@ -80,6 +70,30 @@ class Identifier:
         else:
             raise ValueError('Несоответствие количества выходов и длины массива x. ' +
                              str(len(x)) + ' != ' + str(len(self._model.outputs)))
+
+    def update_a(self, a) -> None:
+        c = self._model.coefficients
+        len_c = len(c)
+        if len(a) != len_c:
+            raise ValueError(f'Длина массива {a} должна быть = {len_c}. {len_c} != {len(a)}')
+        for i in range(len_c):
+            c[i].update(a[i])
+
+    def update_x(self, val) -> None:
+        x = self._model.outputs  # группы
+        len_x = len(x)
+        if len_x != len(val):
+            raise ValueError(f'Длина массива {val} должна быть = {len_x}. {len_x} != {len(val)}')
+        for i in range(len_x):
+            x[i][0].update(val[i])
+
+    def update_u(self, val) -> None:
+        u = self._model.inputs  # группы
+        len_u = len(u)
+        if len_u != len(val):
+            raise ValueError(f'Длина массива {val} должна быть = {len_u}. {len_u} != {len(val)}')
+        for i in range(len_u):
+            u[i][0].update(val[i])
 
     def __repr__(self):
         pass
@@ -132,12 +146,28 @@ def main():
     print('u', iden.u_values)
     print('-' * 20)
     iden.init_a([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    iden.init_x([[6, 5, 4]])
     iden.init_u([[1, 2, 6]])
     print('new_a', iden.a_values)
-    print('x', iden.x_values)
+    print('new_x', iden.x_values)
     print('new_u', iden.u_values)
     print('-' * 20)
     print(iden.min_numb_measurements())
+    print('-' * 20)
+    iden.update_a([1, 2, 3])
+    print('a', iden.a_values)
+    iden.update_x([5])
+    print('x1', iden.x_values)
+    iden.update_x([9])
+    print('x2', iden.x_values)
+    iden.update_u([10])
+    print('u', iden.u_values)
+    print('-' * 20)
+    print(iden.model.last_a)
+    print(iden.model.last_x)
+    print(iden.model.last_u)
+    print('-' * 20)
+    iden.init_x([[6, 5]])
 
 
 if __name__ == '__main__':
