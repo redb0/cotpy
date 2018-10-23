b8ebcf26f4e0138c2943de20fcf1013e8d6038c2
Vladimir <30475117+redb0@users.noreply.github.com>
2018-10-23 19:23:50 +0700

    доработан простейший адаптивный алгоритм

--- identification/alg.py
+++ identification/alg.py
@@ -30,6 +30,7 @@ class Adaptive(Algorithm):
     def __init__(self):
         super().__init__()
 
+    @staticmethod
     def find_a(self, identifier, method, *args, **kwargs):
         if method == 'simplest' or method == 'smp':
             pass
@@ -37,12 +38,24 @@ class Adaptive(Algorithm):
             pass
         elif method == 'pole':
             pass
+        else:
+            raise ValueError('Метода "' + method + '" не существует.')
 
     @staticmethod
-    def simplest(last_a, obj_val, grad):
+    def simplest(last_a, obj_val, grad, gamma=1, g_type='factor'):
         # простейший адаптивный алгоритм
         # TODO: документацию
-        fraction = (obj_val - grad @ last_a) / (grad @ grad)  # скаляр
+        fraction = (obj_val - grad @ last_a)
+        if gamma > 0:
+            if g_type in ['factor', 'f']:
+                fraction *= gamma
+                fraction /= (grad @ grad)  # скаляр
+            elif g_type in ['addend', 'a']:
+                fraction /= (gamma + (grad @ grad))
+            else:
+                raise ValueError('Не найдено типа g_type (' + g_type + ').')
+        else:
+            raise ValueError('Gamma должна быть > 0. ' + str(gamma) + ' <= 0')
         new_a = last_a + fraction * grad  # вектор
         return new_a
 
