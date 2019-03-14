aee32de029e6d3e0797677a6aca7838a47dea228
Vladimir <voronov.volodya2013@yandex.ru>
2019-03-14 14:32:08 +0700

    Удалена папка analyzer

--- cotpy/analyzer/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = "Vladimir Voronov"
-__copyright__ = "Copyright 2018, The CotPy Project"
-__credits__ = ["Vladimir Voronov"]
-__license__ = "BSD 3-Clause License"
-__email__ = "voronov.volodya2013@yandex.ru"
-__status__ = "Development"
